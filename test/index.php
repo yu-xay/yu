63ab5831b51c61dbc68c728e84ba167d186ae0eb
yu <1846721817@qq.com>
2020-01-03 11:30:37 +0800

    add

---
+++ test/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * link: http://www.zjhejiang.com/
+ * copyright: Copyright (c) 2020 浙江禾匠信息科技有限公司
+ * author: xay
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack) - 1]);
+        $this->assertEquals(1, count($stack));
+
+        echo 1;exit;
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
+
+?>
