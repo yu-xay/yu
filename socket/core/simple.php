75d36b719c318f3b311d9e6d388e0e55595716bf
yu <1846721817@qq.com>
2020-01-06 10:51:36 +0800

    simple

---
+++ socket/core/simple.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * link: http://www.zjhejiang.com/
+ * copyright: Copyright (c) 2020 浙江禾匠信息科技有限公司
+ * author: xay
+ */
+
+class simple
+{
+    private static $connectPool = [];  //连接池
+    public function index()
+    {
+
+    }
+}
