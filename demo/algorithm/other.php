4ab61e69e97701b3df37bf64369e658f0f7a8f1d
yu <1846721817@qq.com>
2020-01-09 10:06:27 +0800

    提交个版本

---
+++ demo/algorithm/other.php
@@ -0,0 +1,23 @@
+<?php
+function decimalismtobinary($number, &$arr=[])
+{
+  array_unshift($arr, $number%2);
+  $number = floor($number/2);
+  if ((int)$number === 0) {
+    return;
+  }
+  decimalismtobinary($number, $arr);
+  return implode('', $arr);
+}
+
+function binarytodecimalism($str)
+{
+    $a = 123 >> 3;
+
+  $length = strlen($str)-1;
+  $res = 0;
+  for ($i=$length; $i >= 0; --$i) {
+    $res += $str[$i]*(2**($length-$i));
+  }
+  return $res;
+}
