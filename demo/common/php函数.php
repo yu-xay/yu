e669278e3fd6c3e4c5ec36fe3954df584baaeb55
yu <1846721817@qq.com>
2020-03-14 14:58:34 +0800

    补充

--- demo/common/php函数.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * link: http://www.zjhejiang.com/
- * copyright: Copyright (c) 2020 浙江禾匠信息科技有限公司
- * author: xay
- */
---
+++ doc/PHP/core.md
@@ -0,0 +1,1 @@
+[PHP 内核与原生扩展开发](https://learnku.com/docs/php-internals/php7)
