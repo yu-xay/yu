6d5e842fa3b07a97a63816cf115c0927eb21fb8a
yu <1846721817@qq.com>
2020-06-02 15:44:52 +0800

    索引v1

--- doc/php-func.php
+++ doc/php-func.php
@@ -1,15 +1,26 @@
 <?php
 /*
 base64_encode(string $data):string                      使用MIME base64编码数据
+
 php_sapi_name(void):string                              返回Web服务器和PHP之间的接口类型
-http_build_query(array)
+
+http_build_query(array)                                 数组转字符串
+
 parse_url(string $url, [int $component = -1]):mixed     解析 URL
+
 xdebug_debug_zval('encode');                            引用次数
+
 debug_zval_dump('encode');                              引用次数
+
 realpath(string $path): string                          返回规范化的绝对路径名
+
 getallheaders(void): array                              获取全部 HTTP 请求头信息
+
 array_fill(index,number,value);                         填充数组
 
+usort($array,($current,$next) => {$current <=> $next})  数组自定义排序 (比较函数须返回整数)
+
+array_multisort()
 
 
 
