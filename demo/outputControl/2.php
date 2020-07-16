36859c17c9f32b223f4363c1c82e5c0164a77982
yu <1846721817@qq.com>
2020-07-16 18:22:21 +0800

    url

--- demo/outputControl/2.php
+++ demo/outputControl/2.php
@@ -1,3 +1,10 @@
 <?php
 echo $a;
 echo 'SS';
+https://api.drupal.org/api/drupal/9.0.x/search/theme_render_template
+http://www.php20.cn/article/203
+http://www.php20.cn/article/104
+https://www.cnblogs.com/raobenjun/p/8086051.html
+https://learnku.com/articles/41439
+https://blog.csdn.net/enlyhua/article/details/51004229
+https://www.cnblogs.com/saw2012/archive/2013/01/30/2882451.html
