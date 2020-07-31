a10c2825e2dd2a7e39b1088ba8b2dabc7b53ee81
yu <1846721817@qq.com>
2020-07-31 17:09:41 +0800

    补充

--- demo/features/header.php
+++ demo/features/header.php
@@ -23,12 +23,21 @@ $class = new class {
         $header_array = get_headers($file, true);
         $filesize = $header_array['Content-Length'];
         header('Accept-Length:' . $filesize);
-        $fp = fopen($file, "r") ; //将filename指定的名字资源绑定到一个流上
+        $fp = fopen($file, "r"); //将filename指定的名字资源绑定到一个流上
         while (!feof($fp)) { //测试文件指针是否到了文件结束的位
             $buff = fread($fp, 1024); //读取文件 可安全用于二进制文件
             print_r($buff);
         }
         fclose($fp);
+    }
+
+    //断点续传 基本原理
+    public function blockDown()
+    {
+        header("Range: bytes=0-9");
+        header('HTTP /1.1 206 Partial Content');
+        header('Content-Range: bytes 0-10/100');
+        header('Content-Disposition: attachment; filename=down.binary');
     }
 };
 $class->downBigFile();
