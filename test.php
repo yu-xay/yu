86c928e890b79e1167a813102d0c4e319ed2d227
yu <1846721817@qq.com>
2020-09-18 09:16:56 +0800

    日常提交

--- test.php
+++ test.php
@@ -1,2 +1,21 @@
 <?php
-echo strtoupper('replication');
+require 'vendor/autoload.php';
+
+
+$client = new \GuzzleHttp\Client();
+// 发送一个异步请求
+$request = new \GuzzleHttp\Psr7\Request('GET', 'http://www.sopans.com');
+$request2 = new \GuzzleHttp\Psr7\Request('GET', 'http://www.sopans.com/laruence');
+$request3 = new \GuzzleHttp\Psr7\Request('GET', 'http://www.sopans.com/about');
+$promise = $client->sendAsync($request)->then(function ($response) {
+    sleep(4);
+    echo 111;
+});
+$promise = $client->sendAsync($request2)->then(function ($response)use($client,$request3) {
+    $client->sendAsync($request3)->then(function ($res){
+        echo 444;
+    });
+    echo 222;
+});
+echo 333;
+$promise->wait();
