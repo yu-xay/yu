04f4eab4a89e47ed139f9491fe430d1470314933
yu <1846721817@qq.com>
2020-11-02 11:49:27 +0800

    auto commit

--- test.php
+++ test.php
@@ -1,2 +1,8 @@
 <?php
-phpinfo();
+
+var_dump(ini_get('post_max_size'));exit;
+$redis = new \Redis();
+$redis->connect('redis', 6379);
+echo $redis->set('name', 'va');
+$v = $redis->get('name');
+var_dump($v);
