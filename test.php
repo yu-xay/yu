a9f56c9fb2b4bc2e56de9a83c33e2a3601fe7f1b
yu <1846721817@qq.com>
2020-11-02 16:52:21 +0800

    wamp

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 
-var_dump(ini_get('post_max_size'));exit;
+
 $redis = new \Redis();
-$redis->connect('redis', 6379);
+$redis->connect('yu-redis', 6379);
 echo $redis->set('name', 'va');
 $v = $redis->get('name');
 var_dump($v);
