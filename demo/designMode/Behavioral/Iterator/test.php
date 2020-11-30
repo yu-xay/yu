be0441165a24a7ccef94f770ad98a30b238003d5
1396639186 <1846721817@qq.com>
2020-11-30 16:33:55 +0800

    部分设计模式

--- demo/designMode/Behavioral/Iterator/test.php
+++ demo/designMode/Behavioral/Iterator/test.php
@@ -11,24 +11,6 @@ class Iterator extends TestCase
 {
     public function testIndex()
     {
-        $bookList = new BookList();
-        $bookList->addBook(new Book('Learning PHP Design Patterns', 'William Sanders'));
-        $bookList->addBook(new Book('Professional Php Design Patterns', 'Aaron Saray'));
-        $bookList->addBook(new Book('Clean Code', 'Robert C. Martin'));
-
-        $books = [];
-
-        foreach ($bookList as $book) {
-            $books[] = $book->getAuthorAndTitle();
-        }
-
-        $this->assertSame(
-            [
-                'Learning PHP Design Patterns by William Sanders',
-                'Professional Php Design Patterns by Aaron Saray',
-                'Clean Code by Robert C. Martin',
-            ],
-            $books
-        );
+        $a = new CarList();
     }
 }
